9b91ab3eae03315d9fafa9eebce8f0837251ea43
Pavel Vondra <pavelvondra66@gmail.com>
2024-04-14 20:05:05 +0200

    feat(web): Make layout for desktop

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,9 +8,11 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className="bg-black h-[100vh] overflow-x-hidden">
+      <body className="bg-black h-[100vh] overflow-x-hidden xl:flex xl:pt-8">
         <Navigation />
-        <main className="mx-4 flex flex-col gap-6">{children}</main>
+        <main className="mx-4 flex flex-col gap-6 mt-6 pb-14 md:mx-6">
+          {children}
+        </main>
       </body>
     </html>
   );
--- packages/ui/src/components/navigation/Navigation.tsx
+++ packages/ui/src/components/navigation/Navigation.tsx
@@ -19,16 +19,20 @@ const navigationIcons = {
 
 const Navigation = () => {
   return (
-    <nav className="bg-darkBlue px-4 py-[18px] w-full max-h-fit flex justify-between items-center md:mx-auto md:rounded-[10px] md:mt-6 lg:flex-col lg:h-full">
+    <nav className="bg-darkBlue px-4 py-[18px] max-h-fit flex justify-between items-center md:rounded-[10px] md:mt-6 md:mx-6 xl:flex-col xl:h-[95vh] xl:pt-[35px] xl:pb-8 xl:justify-start xl:gap-[75px] xl:mt-0">
       <Link href={NAV_PATHS.home}>
-        <Image className="w-[25px]" src={logo} alt="Entertainment app logo" />
+        <Image
+          className="w-[25px] xl:w-8"
+          src={logo}
+          alt="Entertainment app logo"
+        />
       </Link>
-      <ul className="inline-flex gap-x-6 items-center lg:flex-col">
+      <ul className="inline-flex gap-x-6 items-center xl:flex-col xl:w-10 xl:flex-grow xl:gap-10">
         {Object.entries(NAV_PATHS).map(([key, value]) => (
           <li key={`${key}-nav-link`}>
             <Link href={value}>
               <Image
-                className="w-4 aspect-square"
+                className="w-4 aspect-square xl:w-5"
                 src={navigationIcons[key as keyof typeof navigationIcons]}
                 alt={`Icon for ${key} navigation link`}
               />
@@ -37,7 +41,7 @@ const Navigation = () => {
         ))}
       </ul>
       <Image
-        className="relative w-6 aspect-square border-[1px] border-white rounded-full"
+        className="relative w-6 aspect-square border-[1px] border-white rounded-full xl:w-10"
         src={avatarPng}
         alt="Avatar user icon"
       />
