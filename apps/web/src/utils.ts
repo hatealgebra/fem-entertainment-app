72cf860b2d306e73d9dc71fc021eeb78126ddbe3
Pavel Vondra <pavelvondra66@gmail.com>
2024-05-26 11:05:46 +0200

    feat(web): Create utils for getting the token secret

--- apps/web/src/utils.ts
+++ apps/web/src/utils.ts
@@ -4,3 +4,19 @@ export const getSearchParam = (req: NextRequest, key: string) => {
   const paramValue = req.nextUrl.searchParams.get(key);
   return paramValue;
 };
+
+export const getTokenSecret = async (
+  tokenType: "refresh" | "access" = "access"
+): Promise<Uint8Array | Error> => {
+  const tokenTypeUpper = tokenType.toUpperCase();
+  const envVars = await import("./helpers/server/envVars");
+  const tokenSecret = envVars[`${tokenTypeUpper}_TOKEN_SECRET`];
+
+  if (!tokenSecret) {
+    throw new Error(`Missing token ${tokenType} secret`);
+  }
+
+  const encodedTokenSecret = new TextEncoder().encode(tokenSecret);
+
+  return encodedTokenSecret;
+};
