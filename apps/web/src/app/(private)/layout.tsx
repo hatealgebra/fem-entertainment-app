d866ffc5e82f83ab6f3543b9ddb2cd39c710203f
Pavel Vondra <pavelvondra66@gmail.com>
2024-09-29 20:08:03 +0200

    feat(app): Handle the dialog opened in the body

--- apps/web/src/app/(private)/layout.tsx
+++ apps/web/src/app/(private)/layout.tsx
@@ -12,9 +12,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ContextProvider>
-        <body className="relative bg-black overflow-x-hidden xl:flex xl:pt-8">
+        <body className="relative bg-black overflow-x-hidden xl:flex xl:pt-8 [&:has(dialog[open])]:overflow-hidden">
           <Navigation />
-          <main className="mx-4 min-w-0 max-w-full flex flex-col gap-6 mt-6 pb-14 md:mx-6 w-full">
+          <main className="mx-4 min-w-0 max-w-full flex flex-col gap-6 mt-6 pb-14 md:mx-6">
             <SearchForm />
             {children}
           </main>
