7c2a9ed6f28616cdce6e096e3c0b1a4c1fdbff39
Pavel Vondra <pavelvondra66@gmail.com>
2024-11-29 17:45:51 +0100

    feat(database): Update movie model after updating the dataset

--- packages/database/src/lib/models/movie.ts
+++ packages/database/src/lib/models/movie.ts
@@ -1,37 +1,84 @@
 import mongoose, { Schema, model } from "mongoose";
-import { ERating, IMovie, ECategory } from "@repo/misc/types/movies.d.ts";
-
-const MovieSchema = new Schema<IMovie>({
+import { IMovieBackend } from "@repo/misc/types/movies.d.ts";
+const MovieSchema = new Schema<IMovieBackend>({
+  _id: {
+    type: String,
+  },
+  adult: {
+    type: Boolean,
+  },
+  budget: {
+    type: Number,
+  },
+  genres: {
+    id: Number,
+    name: String,
+  },
+  id: {
+    type: Number,
+  },
+  imdb_id: {
+    type: String,
+  },
+  original_language: {
+    type: String,
+  },
+  original_title: {
+    type: String,
+  },
+  overview: {
+    type: String,
+  },
+  popularity: {
+    type: Number,
+  },
+  poster_path: {
+    type: String,
+  },
+  production_companies: {
+    id: Number,
+    name: String,
+  },
+  production_countries: {
+    id: Number,
+    name: String,
+  },
+  release_date: {
+    id: Number,
+    name: String,
+  },
+  revenue: {
+    type: Number,
+  },
+  runtime: {
+    type: Number,
+  },
+  spoken_languages: {
+    iso_639_1: String,
+    name: String,
+  },
+  status: {
+    type: String,
+  },
+  tagline: {
+    type: String,
+  },
   title: {
     type: String,
   },
-  thumbnail: {
-    trending: {
-      small: String,
-      large: String,
-    },
-    regular: {
-      small: String,
-      medium: String,
-      large: String,
-    },
+  video: {
+    type: Boolean,
   },
-  year: {
+  vote_average: {
     type: Number,
   },
-  category: {
-    type: String,
-    enum: ECategory,
+  vote_count: {
+    type: Number,
   },
-  rating: {
-    type: String,
-    enum: ERating,
-  },
-  isTrending: { type: Boolean, default: false },
 });
 
 const Movie =
-  (mongoose.models?.Movie as mongoose.Model<IMovie>) ||
+  (mongoose.models?.Movie as mongoose.Model<IMovieBackend>) ||
   model("Movie", MovieSchema);
 
 export default Movie;
--- packages/misc/src/types/movies.d.ts
+++ packages/misc/src/types/movies.d.ts
@@ -1,34 +1,69 @@
-export interface IMovie {
-  _id: string;
-  title: string;
-  thumbnail: IThumbnail;
-  year: number;
-  category: ECategory;
-  rating: ERating;
-  isTrending: boolean;
-}
-export interface IThumbnail {
-  trending?: ITrending | null;
-  regular: IRegular;
-}
-export interface ITrending {
-  small: string;
-  large: string;
-}
-export interface IRegular {
-  small: string;
-  medium: string;
-  large: string;
+export interface IMovie
+  extends Pick<
+    IMovieBackend,
+    | "_id"
+    | "adult"
+    | "budget"
+    | "genres"
+    | "id"
+    | "overview"
+    | "popularity"
+    | "revenue"
+    | "runtime"
+    | "status"
+    | "tagline"
+    | "title"
+    | "video"
+  > {
+  imdbId: string;
+  originalLanguage: string;
+  originalTitle: string;
+  posterPath: string;
+  productionCompanies: IProductionCompany[];
+  productionCountries: IProductionCountry[];
+  releaseDate: string;
+  spokenLanguages: ISpokenLanguage[];
+  voteAverage: number;
+  voteCount: number;
 }
 
-export enum ERating {
-  PG = "PG",
-  E = "E",
-  EIGHTEEN_PLUS = "18+",
-  N_A = "N/A",
+export interface IMovieBackend {
+  _id: string;
+  adult: boolean;
+  budget: number;
+  genres: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: IProductionCompany[];
+  production_countries: IProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: ISpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
-export enum ECategory {
-  MOVIE = "Movie",
-  TV_SERIES = "TV Series",
-  N_A = "N/A",
+
+export interface IProductionCompany {
+  id: number;
+  name: string;
+}
+
+export interface IProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface ISpokenLanguage {
+  iso_639_1: string;
+  name: string;
 }
