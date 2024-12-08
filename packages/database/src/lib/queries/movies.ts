bd6e6dfd7efdb2687671d0c70ed2e397ff019b5a
Pavel Vondra <pavelvondra66@gmail.com>
2024-12-08 22:24:48 +0100

    feat(db): add more movie queries

--- packages/database/src/lib/queries/movies.ts
+++ packages/database/src/lib/queries/movies.ts
@@ -1,9 +1,6 @@
-import dbConnect from "../dbConnection";
 import Movie from "../models/movie";
 
 export const getTrending = async () => {
-  await dbConnect();
-
   const trendingMovies = await Movie.find({
     adult: false,
     popularity: { $gte: 100 },
@@ -16,4 +13,45 @@ export const getTrending = async () => {
   const moviesArray = Object.values(trendingMovies);
 
   return moviesArray;
+};
+
+export const getBestRated = async () => {
+  const bestRatedMovies = await Movie.find({
+    adult: false,
+    vote_count: { $gte: 20000 },
+    vote_average: { $gte: 7 },
+  })
+    .sort({ vote_average: -1 })
+    .limit(12)
+    .lean()
+    .exec();
+
+  return bestRatedMovies;
+};
+
+export const getThisYear = async () => {
+  const thisYearMovies = await Movie.find({
+    adult: false,
+    release_date: { $gte: new Date(2023, 1, 1), $lt: new Date(2024, 1, 1) },
+    vote_count: { $gte: 1000 },
+  })
+    .sort({ vote_count: -1 })
+    .lean()
+    .limit(10);
+
+  return thisYearMovies;
+};
+
+export const getPlanned = async () => {
+  const plannedMovies = await Movie.find({
+    adult: false,
+    status: "Planned",
+    backdrop_path: { $exists: true },
+    poster_path: { $exists: true },
+  })
+    .sort({ popularity: -1 })
+    .lean()
+    .limit(10);
+
+  return plannedMovies;
 };
